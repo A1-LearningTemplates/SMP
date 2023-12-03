448e6230388d05ee3a0a58569b9e04d0e4dcbb4a
AnasAlkhamis <anas.alkhamnis@gmail.com>
2023-12-03 20:59:35 +0300

    build2

--- convex/schema.ts
+++ convex/schema.ts
@@ -12,14 +12,15 @@ export default defineSchema({
     media: v.optional(v.string()),
     title: v.string(),
     userId: v.id("users"),
-    user: v.optional(
-      v.object({
-        _id: v.id("users"),
-        email: v.string(),
-        is_active: v.boolean(),
-        picture: v.string(),
-        nickname: v.string(),
-      })
-    ),
+    // user: v.optional(
+    //   v.object({
+    //     _id: v.id("users"),
+    //     email: v.string(),
+    //     is_active: v.boolean(),
+    //     picture: v.string(),
+    //     nickname: v.string(),
+    //     _creationTime: v.number(),
+    //   })
+    // ),
   }),
 });

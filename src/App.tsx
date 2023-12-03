5ed1fc674ac222c4202b5ed0c10ff61080197494
AnasAlkhamis <anas.alkhamnis@gmail.com>
2023-12-03 21:28:37 +0300

    build4

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,23 @@ import "./App.css";
 
 import NavBar from "./components/NavBar";
 import { RouterProvider } from "react-router-dom";
-import authRouter from "./routes/authRoutes";
+// import authRouter from "./routes/authRoutes";
 import unAuthRouter from "./routes/unAuthRoutes";
-import { Authenticated, Unauthenticated } from "convex/react";
+import { /* Authenticated, */ Unauthenticated } from "convex/react";
 
 function App() {
-  console.log("rerenderd");
-
   return (
     <div className="px-1 lg:px-20 xl:px-24 bg-slate-200">
       <NavBar />
       <Unauthenticated>
         <RouterProvider router={unAuthRouter} />
       </Unauthenticated>
-      <Authenticated>
+      {/* <Authenticated>
           <RouterProvider
             router={authRouter}
             fallbackElement={<h1 className=" text-2xl">loading..</h1>}
           />
-      </Authenticated>
+      </Authenticated> */}
     </div>
   );
 }

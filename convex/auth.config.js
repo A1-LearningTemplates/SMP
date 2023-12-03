8ccbe74d8272dbacca2ec0f4bde533407d79c010
AnasAlkhamis <anas.alkhamnis@gmail.com>
2023-12-03 22:31:25 +0300

    build 9

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,13 @@
 export default {
   providers: [
+    // {
+    //   domain: "dev-72p0wpqqdl8dfn8n.us.auth0.com",
+    //   applicationID: "iN3NjtnW9O0UpN4ltXtIikFMzRTONx4h",
+    // },
     {
-      domain: "dev-72p0wpqqdl8dfn8n.us.auth0.com",
-      applicationID: "iN3NjtnW9O0UpN4ltXtIikFMzRTONx4h",
+      domain: "dev-l4z62eb5u5zkd01m.us.auth0.com",
+      applicationID: "ygmWWAR3gUHp09Ec0F5A0aPs2FWvo0Mt",
     },
   ],
 };
+// dev-l4z62eb5u5zkd01m.us.auth0.com

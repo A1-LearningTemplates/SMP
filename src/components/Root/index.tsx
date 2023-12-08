0b8f3459e523959bdc69f9ea7b2b9621bf79392f
AnasAlkhamis <anas.alkhamnis@gmail.com>
2023-12-08 15:19:38 +0300

    change path on refresh

--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -1,16 +1,19 @@
-import { useNavigate, Outlet } from "react-router-dom";
+import { useNavigate, Outlet, useLocation } from "react-router-dom";
 import NavBar from "../NavBar";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
 
 const Root = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const { isAuthenticated, isLoading } = useAuth0();
   useEffect(() => {
     if (!isAuthenticated && !isLoading) {
       navigate("/");
+    } else {
+      navigate(location.pathname);
     }
-  }, []);
+  }, [isAuthenticated, isLoading]);
 
   return (
     <>
